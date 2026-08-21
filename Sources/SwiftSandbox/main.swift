c4184a3920d42a07408d5ac3906a9634ce7d83fc
gi6star <dgeovanamoreira@gmail.com>
2026-08-21 14:49:45 -0300

    mudança de cards.swift para main.swift

--- Sources/SwiftSandbox/main.swift
+++ Sources/SwiftSandbox/main.swift
@@ -1,1 +1,83 @@
-print("Geovana!")
+let cards = [1, 2, 3]
+
+/// Obtém a carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - index: O índice da carta que será obtida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: A carta localizada no índice especificado.
+func getCard(at index: Int, from stack: [Int]) -> Int {
+    if index < stack.count { //
+        return stack[index] // retorna o card no index especifico
+    } else {
+        return -1
+    }
+}
+/// Substitui a carta existente no índice especificado por uma nova carta.
+///
+/// - Parameters:
+///   - index: O índice da carta que será substituída.
+///   - stack: A pilha que contém a carta a ser substituída.
+///   - newCard: A nova carta que substituirá a carta existente.
+/// - Returns: Uma nova pilha com a carta no índice especificado substituída por `newCard`.
+
+func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
+    if index < stack.count {
+        var copy = stack
+        copy[index] = newCard
+        return copy
+    } else {
+        return stack
+    }
+}
+/// Insere uma nova carta no topo da pilha.
+///
+/// - Parameters:
+///   - newCard: A carta que será inserida.
+///   - stack: A pilha onde a carta será inserida.
+/// - Returns: Uma nova pilha com a carta inserida no topo.
+func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
+    var copy = stack
+    copy.append(newCard)
+    return copy
+}
+
+/// Remove a carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - index: O índice da carta que será removida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: Uma nova pilha sem a carta removida.
+func removeCard(at index: Int, from stack: [Int]) -> [Int] {
+  var copy = stack
+    copy.remove(at: index)
+    return copy
+}
+/// Insere uma nova carta no índice especificado da pilha.
+///
+/// - Parameters:
+///   - newCard: A carta que será inserida.
+///   - index: O índice onde a carta será inserida.
+///   - stack: A pilha que contém as cartas.
+/// - Returns: Uma nova pilha com a carta inserida no índice especificado.
+func insert(_ newCard: Int, at index: Int, from stack: [Int]) -> [Int] {
+  var copy = stack
+  copy.insert(newCard, at: index)
+  return copy
+}
+
+/// Verifica se a pilha possui a quantidade especificada de cartas.
+///
+/// - Parameters:
+///   - stack: A pilha que será verificada.
+///   - size: A quantidade esperada de cartas.
+/// - Returns: `true` se a pilha tiver a quantidade especificada de cartas; caso contrário, `false`.
+func checkSizeOfStack(_ stack: [Int], _ size: Int) -> Bool {
+    return stack.count == size
+}
+// ou...
+//    if stack.count == size {
+//        return true
+//    } else {
+//        return false
+//    }
